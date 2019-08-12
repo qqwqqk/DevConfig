44b2ec37dbc018cf044d53296f451984023bf414
qqwqqk <qqwqqk@shu.edu.cn>
2019-08-12 13:45:41 +0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdlib>
+#include "header/func.h"
+using namespace std;
+
+int main()
+{
+  int a,b,c;
+  a=2;
+  b=3;
+  c=add(a,b);
+  
+  cout << c<< endl;
+  return 0;
+}
