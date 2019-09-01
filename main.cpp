606393ed8354cabc62a440083bc585cc4dd52357
qqwqqk <qqwqqk@shu.edu.cn>
2019-09-01 21:39:15 +0800

    add

--- main.cpp
+++ main.cpp
@@ -1,57 +1,81 @@
 #include <iostream>
-#include <set>
+#include <stack>
+#include <map>
 #include <string>
 #include <vector>;
 using namespace std;
 
-set<int> getpossible(int max, int id, vector<int> pos){
-    set<int> possible;
-    vector<int> prestep;
-    vector<int> nextstep;
-    
-    prestep.push_back(id);
-    
-    for(int i=0; i<pos.size(); i++){
-        int step = pos[i];
-        for(int j=0; j<prestep.size(); j++){
-            int curr = prestep[i];
-            if(curr - step > 0){
-                possible.insert(curr - step); 
-                nextstep.push_back(curr - step);
-            }
-            if(curr + step <= max){
-                possible.insert(curr + step); 
-                nextstep.push_back(curr + step);
-            }
-        }
-        prestep = nextstep;
-        nextstep.clear();
-    }
-    return possible;
-}
+class company{
+public:
+	int code;
+	vector<int> price;
+	company(int _code, int _price){
+		code = _code;
+		price.push_back(_price);
+	};
+};
+
+map<int, int> getresult(vector<int> xlist, map<int, company> lists){
+	int company_id, company_price;
+	int maxval = 0;
+	for (int i = 0; i < xlist.size(); i++){
+		company iter = lists.find(xlist[i])->second;
+		int temp = iter.price.back();
+		maxval = maxval > temp ? maxval : temp;
+		lists.erase(xlist[i]);
+	}
+
+	int max = 0;
+	if (lists.size() > 0){
+		for (map<int, company>::iterator iter = lists.begin(); iter != lists.end(); iter++){
+			int val = iter->second.price.back();
+			if (max <= val){ 
+				max = val; 
+				company_id = iter->first;
+				company_price = val;
+			}
+		}
+	}
+
+	map<int, int> res;
+	res.insert(pair<int, int>(company_id, company_price));
+	return res;
+};
 
 int main(){
-    
-    int N,M,D;
-    vector<int> listD;
-    
-	cin >> N >> M;
-    
-    for(int i=0; i<M; i++){
-        cin >> D;
-        listD.push_back(D);
-    }
-    
-    set<int> result;
-    set<int> cache;
-    for(int i = 1; i<= M; i++){
-        cache = getpossible(N, i, listD);
-        for(set<int>::iterator item = cache.begin(); item!= cache.end(); item++){
-            result.insert(*item);
-        }
-    }
-    
-	cout << result.size() << endl;
+
+	int n, q;
+	map<int, company> lists;
+
+	cin >> n;
+	int num = 0, price = 0;
+
+	for (int i = 0; i<n; i++){
+		cin >> num >> price;
+		map<int, company>::iterator iter = lists.find(num);
+		if (iter == lists.end()){
+			company val(num, price);
+		}
+		else {
+			iter->second.price.push_back(price);
+		}
+	}
+
+	cin >> q;
+	int leng = 0;
+	vector<int> xlist;
+	int max = 0;
+	for (int i = 0; i<n; i++){
+		cin >> leng;
+		for (int i = 0; i<leng; i++){
+			cin >> num;
+			xlist.push_back(num);
+		}
+
+		map<int, int> result = getresult(xlist, lists);
+		map<int, int>::iterator temp = result.begin();
+		cout << temp->first << ' ' << temp->second << endl;
+	}
 
 	return 0;
 }
